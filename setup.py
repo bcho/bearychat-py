80fe3770f159123b0a22e53a3e8f8d77b7113326
hbc <bcxxxxxx@gmail.com>
2015-08-18 15:28:42 +0800

    Update build process.

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 # coding: utf-8
 
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
 install_requires = [
     'requests'
 ]
+
+
+class PyTest(TestCommand):
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -17,9 +36,13 @@ setup(
     description='A simple package for interacting with bearychat.',
     long_description=open('README.md').read(),
     license='MIT',
-    packages=find_packages('.'),
+    packages=find_packages('.', exclude=['tests*']),
     zip_safe=False,
     install_requires=install_requires,
+
+    cmdclass={'test': PyTest},
+    tests_require=['pytest', 'responses'],
+
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
